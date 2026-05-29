2d0390927577b7570840be5676d0ec1c55cb6334
MamaniRamosJeanMarcos <xelmodioss@gmail.com>
2026-05-29 15:48:32 -0500

    Agregar metodo suma

--- Program.cs
+++ Program.cs
@@ -6,5 +6,12 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static double Suma(double num1, double num2)
+        {
+            double resultado = num1 + num2;
+            return resultado;
+        }
+
     }
 }
